6de468cee7e5f8173ae9e5cbeaccfa7db613a67f
Chethan2k1 <40890937+Chethan2k1@users.noreply.github.com>
2019-12-14 14:59:09 +0530

    Chat backend

---
+++ client.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3.6
+
+import socket
+import sys
+import threading
+
+name=input("Enter your Username : ")
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+host='127.0.0.1'
+port=1235
+s.connect((host,port))
+s.setblocking(True)
+
+
+def msg_input():
+    print('waiting for input')
+    while True:
+        msg=input('> ')
+        if len(msg)!=0:
+            new_msg='@'+name+'> '+msg
+            s.send(new_msg.encode('utf-8'))
+        else:
+            continue
+
+def msg_read():
+    print('reading')
+    while True:
+            data=s.recv(1024).decode('utf-8')
+            if(len(data)!=0):
+                print(data)
+            else: 
+                continue
+    
+threading.Thread(target=msg_input).start()
+threading.Thread(target=msg_read).start()
---
+++ server.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+import pymysql
+import socket
+import threading
+import queue
+
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+socket.socket(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+host='127.0.0.1'
+port=1235
+server.bind((host,port))
+server.listen()
+
+conn=pymysql.connect("localhost","root","Pingpong@123","chat")
+cursor=conn.cursor()
+
+t=[]
+users=0
+print('Online:'+str(users)+'\n')
+
+def read(connection,q):
+    try:
+        while True:
+            data=connection.recv(1024).decode('utf-8')
+            print(data)
+            q.put(data)
+            #print(data.split('>')[0].split('@')[1])
+            #print(data[(len(data.split('>')[0])+2):])
+            sql_query="INSERT INTO chats(name,msg) VALUES('{0}','{1}')".format(data.split('>')[0].split('@')[1],data[(len(data.split('>')[0])+2):])
+            print(sql_query)
+            try:
+                cursor.execute(sql_query)
+                conn.commit()
+            except Exception as e:
+                print(f'Exception:{e}')
+    except Exception:
+        pass
+
+def write(connection,q):
+    global users
+    try:
+        while True:
+            if(q.empty()==False):
+                data=q.get()
+                if len(data)!=0:
+                    for a in t:
+                        if a!=connection:
+                            #print('Sent:{0} Sent By:{1}'.format(data,data.split('>')[0].split('@')[1]))
+                            a.send(data.encode('utf-8'))
+                else:
+                    break
+            else:
+                continue
+    except Exception:
+        pass
+    finally:
+        print('client Diconnected')
+        users-=1
+        t.remove(connection)
+        print('Online:'+str(users)+'\n')
+        connection.close()
+        
+def client_thread(connection):
+    global users
+    users+=1
+    print('Online:'+str(users)+'\n')
+    q=queue.Queue()
+    threading.Thread(target=read,args=(connection,q),daemon=True).start()
+    threading.Thread(target=write,args=(connection,q),daemon=True).start()    
+
+while True:
+    try:
+        connection, address=server.accept()
+        t.append(connection)
+        threading.Thread(target=client_thread,args=(connection,),daemon=True).start()
+    except:
+        break
+
+connection.close()
+server.close()
+
