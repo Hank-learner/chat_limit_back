f3fbcfcac39d76a077e909d98fb7d0ee74ff2ed2
Hank-learner <jerryrocker6@gmail.com>
2019-12-15 10:20:09 +0530

    flask hoster

---
+++ app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+import logging
+from flask_mysqldb import MySQL
+from sentiment import sentiment
+import sys
+from flask_cors import CORS
+
+# from auth import authentication
+app = Flask(__name__)
+app.debug = True
+CORS(app)
+
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "password"
+app.config["MYSQL_DB"] = "chat"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+# mysql initialization
+mysql = MySQL(app)
+
+
+@app.route("/")
+def index():
+    mood = sentiment("elements are great")
+    return mood
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    if request.method == "POST":
+        usernamegot = request.form["user"]
+        passwordgot = request.form["pass"]
+
+        output = "failure"
+
+        cursor = mysql.connection.cursor()
+        result = cursor.execute(
+            "SELECT  * from users where name='{0}'".format(usernamegot)
+        )
+
+        if result > 0:
+            data = cursor.fetchone()
+            password = data["password"]
+            password = str(password)
+
+            if password == passwordgot:
+                output = "success"
+
+        return {"message": output}
+    else:
+        return "You are not supposed to be here: You have registered to be hacked by your activity"
+
+
+if __name__ == "___main__":
+    app.secret_key = "secret123"
+    app.run(debug=True,host= '0.0.0.0')
